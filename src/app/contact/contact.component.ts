e06adf058ef5450e9cbf82e7e644237d27508bae
Kavya_Gunji <kavyagunji39@gmail.com>
2026-04-30 14:52:20 +0530

    New Changes Added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,20 +8,25 @@ import { NgForm } from '@angular/forms';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  constructor(private http: HttpClient) {}
-sendMessage(form: NgForm) {
-  if (form.invalid) return;
-
-  this.http.post('https://messiah-backend.onrender.com/send-email', form.value)
-    .subscribe({
-      next: () => {
-        alert('Message sent successfully!');
+  email!: string
+  Message: string = ""
+  constructor(private http: HttpClient) { }
+  sendMessage(form: NgForm) {
+    if (form.invalid) return;
+    this.Message = 'Message sending please wait...'
+    this.http.post('https://messiah-backend.onrender.com/send-email', form.value)
+      .subscribe({
+        next: () => {
+          this.Message = "Message sent successfully!..."
           form.resetForm();
-      },
-      error: (err) => {
-        console.error(err);
-        alert(err.error?.error || 'Failed to send');
-      }
-    });
-}
+          setTimeout(() => {
+            this.Message = ""
+          }, 1500);
+        },
+        error: (err) => {
+          console.error(err);
+          alert(err.error?.error || 'Failed to send');
+        }
+      });
+  }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,25 +8,67 @@ import { Form } from '@angular/forms';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  countryCode: string = '+91';
+  phone = '';
+  phonePattern = '^[6-9]\\d{9}$'; // default India
+  Message: string = ""
+  email!: string
+  constructor(private http: HttpClient) { }
 
+  currentYear: number = new Date().getFullYear();
 
-constructor(private http: HttpClient) {}
+  sendPrayer(form: any) {
 
-currentYear: number = new Date().getFullYear();
+    console.log("FORM INVALID:", form.invalid);
+    console.log("FORM VALUE:", form.value);
 
-   sendPrayer(form: any) {
-    if (form.invalid) 
-
-    this.http.post('https://messiah-backend.onrender.com/send-email', form.value)
+    if (form.invalid) {
+      alert('Please fill all required fields');
+      return;
+    }
+    this.Message = 'Message sending please wait...'
+    this.http.post('https://messiah-backend-1.onrender.com/send-email', form.value)
       .subscribe({
-        next: () => {
-          alert('Message sent successfully!');
-          form.reset();
+        next: (res) => {
+          console.log("SUCCESS:", res);
+          this.Message = "Message sent successfully!..."
+          form.resetForm({
+            countryCode: '+91'
+          });
+          setTimeout(() => {
+            this.Message = ""
+          }, 1500);
         },
         error: (err) => {
-          console.error("SERVER ERROR:", err);
-          alert(err.error?.error || 'Failed to send');
+          console.error("ERROR:", err);
+          alert('Failed to send');
         }
       });
+  }
+
+  updatePattern() {
+    switch (this.countryCode) {
+      case '+91':
+        this.phonePattern = '^[6-9]\\d{9}$';
+        break;
+
+      case '+1':
+        this.phonePattern = '^\\d{10}$';
+        break;
+
+      case '+44':
+        this.phonePattern = '^\\d{10,11}$';
+        break;
+
+      case '+61':
+        this.phonePattern = '^\\d{9}$';
+        break;
+
+      default:
+        this.phonePattern = '^\\d{6,14}$';
+    }
+
+    // clear old value when switching country
+    this.phone = '';
   }
 }
