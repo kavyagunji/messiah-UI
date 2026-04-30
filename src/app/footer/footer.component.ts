f9ad7ae40920e9781fdba8b06987c60278be320c
kavyagunji <168633315+kavyagunji@users.noreply.github.com>
2026-04-30 06:48:06 +0530

    Update footer.component.ts

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -15,21 +15,25 @@ constructor(private http: HttpClient) {}
 currentYear: number = new Date().getFullYear();
 
    sendPrayer(form: any) {
-    if (form.invalid) {
+
+  console.log("FORM INVALID:", form.invalid);
+  console.log("FORM VALUE:", form.value);
+
+  if (form.invalid) {
     alert('Please fill all required fields');
-    return; // 🔥 stops execution
+    return;
   }
 
-    this.http.post('https://messiah-backend.onrender.com/send-email', form.value)
-      .subscribe({
-        next: () => {
-          alert('Message sent successfully!');
-          form.reset();
-        },
-        error: (err) => {
-          console.error("SERVER ERROR:", err);
-          alert(err.error?.error || 'Failed to send');
-        }
-      });
-  }
+  this.http.post('https://messiah-backend.onrender.com/send-email', form.value)
+    .subscribe({
+      next: (res) => {
+        console.log("SUCCESS:", res);
+        alert('Message sent successfully!');
+        form.reset();
+      },
+      error: (err) => {
+        console.error("ERROR:", err);
+        alert('Failed to send');
+      }
+    });
 }
